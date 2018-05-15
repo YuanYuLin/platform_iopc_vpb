6acc94943fac5383887e99479293a4a407a96822
yyl <yuanyu.lin@gmail.com>
2018-05-15 15:06:59 +0800

    MAJOR_NUM=1 MINOR_NUM=2 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -4,22 +4,24 @@ import iopc
 pkg_path = ""
 output_dir = ""
 arch = ""
-src_lib_dir = ""
-dst_lib_dir = ""
-pkg_sysroot_tarball = ""
-pkg_sysroot_tarball_dir = ""
+output_platform_dir = ""
+src_image_cfg = ""
+dao_file = "dao.ini"
+dao_script = "dao.py"
 
 def set_global(args):
     global pkg_path
     global output_dir
     global arch
-    global src_lib_dir
-    global dst_lib_dir
-    global pkg_sysroot_tarball
+    global src_image_cfg
+    global output_platform_dir
 
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
     arch = ops.getEnv("ARCH_ALT")
+    src_image_cfg = ops.path_join(pkg_path, dao_file)
+    output_platform_dir = ops.path_join(iopc.getOutputRootDir(), "platform")
+    #dst_include_dir = ops.path_join("include",args["pkg_name"])
 
 def MAIN_ENV(args):
     set_global(args)
@@ -28,6 +30,11 @@ def MAIN_ENV(args):
 
 def MAIN_EXTRACT(args):
     set_global(args)
+
+    ops.mkdir(output_platform_dir)
+    ops.copyto(src_image_cfg, ops.path_join(output_platform_dir, dao_file))
+    ops.copyto(ops.path_join(pkg_path, dao_script), output_dir)
+    ops.copyto(ops.path_join(pkg_path, dao_file), output_dir)
 
     return True
 
@@ -48,12 +55,17 @@ def MAIN_CONFIGURE(args):
 def MAIN_BUILD(args):
     set_global(args)
 
-    return True
+    CMD=['python', dao_script, dao_file]
+    ops.execCmd(CMD, output_dir, False)
+
+    return False
 
 def MAIN_INSTALL(args):
     set_global(args)
 
     #iopc.installBin(args["pkg_name"], ops.path_join(dst_usr_lib_dir, "."), "usr/lib")
+    ops.copyto(ops.path_join(output_dir, 'db_init.bin'), iopc.getOutputRootDir())
+    #iopc.installBin(args["pkg_name"], ops.path_join(output_dir, "db_init.inc"), dst_include_dir)
 
     return False
 
---
+++ dao.py
@@ -0,0 +1,113 @@
+#!/bin/python2.7
+
+import struct
+import pprint
+import sys
+import ConfigParser
+import binascii
+
+'''
+[Boot disk layout]
+'''
+
+def create_buffer(record_count, record_size):
+    buff=[]
+    for idx in range(0, record_count):
+        buff.append(bytearray('\0' * record_size))
+
+    return buff
+
+def dao_to_buffer(dao, buff, record_format, data_size):
+    g_index=0
+    for key in dao.keys():
+        pad = bytearray(str.encode('\0' *  data_size))
+
+        idx=0
+        for ch in bytearray(key):
+            pad[idx]=ch
+            idx+=1
+
+        for ch in bytearray(dao[key]):
+            pad[idx]=ch
+            idx+=1
+
+        crc32 = 0
+        rec_type = 1
+        key_len = len(key)
+        val_len = len(dao[key])
+        resv = 0
+        data = struct.pack(record_format, crc32, rec_type, key_len, val_len, resv, str(pad))
+        crc32 = binascii.crc32(bytearray(data))
+
+        data = struct.pack(record_format, 0, rec_type, key_len, val_len, resv, str(pad))
+
+        idx=0
+        for ch in bytearray(data):
+            buff[g_index][idx] = ch
+            idx+=1
+        g_index+=1
+
+    return buff
+
+def out_to_binary(buff):
+    #create_buffer(1, (64 * 1024))
+    #struct.pack("12sH50s", "$[IOPC_DATA]", record_count, bytearray('\0' * 50))
+    fd=open('db_init.bin', 'wb')
+    for rec in buff:
+        fd.write(rec)
+    fd.close()
+
+def out_to_c(buff, record_count, record_format):
+    ft=open('db_init.inc', 'w')
+    ft.write('#define DAO_KV_MAX ' + str(record_count) + '\n')
+    ft.write('static struct dao_kv_512_t dao_kv_list[DAO_KV_MAX]={')
+    idx=0
+    for rec in buff:
+        csum, tp, kl, vl, rsv, data = struct.unpack(record_format, rec)
+        ft.write('\n' + '{')
+        ft.write(str(int(csum)))
+        ft.write(',')
+        ft.write(str(int(tp)))
+        ft.write(',')
+        ft.write(str(int(kl)))
+        ft.write(',')
+        ft.write(str(int(vl)))
+        ft.write(',')
+        ft.write(str(int(rsv)))
+        ft.write(',{')
+        for ch in bytearray(data):
+            ft.write(str(int(ch)))
+            ft.write(',')
+        ft.write('},')
+        ft.write('},')
+    ft.write('};')
+    ft.close()
+
+def help():
+    print "usage: dao.py <dao.ini>"
+    sys.exit(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+    dao={}
+    cfg_ini = sys.argv[1]
+    config = ConfigParser.RawConfigParser()
+    config.read(cfg_ini)
+    single_section = config.items("CFG_DAO")
+    for item in single_section:
+        print "key = %s, valule = %s" % (item[0], item[1])
+        key = item[0]
+        val = item[1]
+        dao[key] = val
+
+    header_size = 12
+    data_size = 500
+    record_count=128
+    record_format="IBBHI" + str(data_size) + "s"
+    record_size= header_size + data_size
+
+    buf=create_buffer(record_count, record_size)
+    buf=dao_to_buffer(dao, buf, record_format, data_size)
+    out_to_binary(buf)
+    out_to_c(buf, record_count, record_format)
